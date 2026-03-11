a51595117630761d2b1e21bba850e5705a839add
mikimah <trinhnhatmiki@gmail.com>
2026-03-11 09:40:06 +0700

    fix add handle SmD12 2

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/', //http://127.0.0.1:8000
+  baseURL: 'https://webbanbanhbe.onrender.com/api/', //http://127.0.0.1:8000
   withCredentials: true,
   headers: {
     'Accept': 'application/json',
